bb5b5c304b69b0acf4492765408fdd4853b1df51
MOISECHRIST <moisechristianjunior464@gmail.com>
2025-11-07 00:42:40 +0100

    Adding path to refseq in config file.

--- scripts/00-configs.sh
+++ scripts/00-configs.sh
@@ -6,7 +6,7 @@
 ## Description : In this configuration file, I will set up all the global paths and variables that will be used in 
 ##                this project and import all the useful modules needed for it.
 ## Creation date : 02-11-2025
-## Last Update : 06-11-2025
+## Last Update : 07-11-2025
 ##------------------------------------------------------------------------
 
 ##------------------------------------------------------------------------
@@ -30,7 +30,7 @@ fi
 ## Set up path and variables for our dataset and metadata
 ##------------------------------------------------------------------------
 
-#Define the path to our dataset directory
+#Define the path to our data directory
 DATA_DIR=${WORKING_DIR}/data
 
 if [ ! -d "$DATA_DIR" ]; then 
@@ -48,6 +48,31 @@ SAMPLE_ID_LIST=($(ls $DATASET_DIR/*.fastq.gz | cut -d"/" -f8 | cut -d_ -f 1 | so
 
 #Define the path to metadata file
 METADATA_FILE=${DATA_DIR}/metadata.csv
+
+#Define path to reference fasta, gtf and gff3 files 
+REFSEQ_FASTA="$DATA_DIR"/refseq/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
+
+#Check if there is REFSEQ_FASTA file 
+if [ ! -f "$REFSEQ_FASTA" ]; then 
+    echo "Please provide the refseq (fasta file) in this path :: $REFSEQ_FASTA"
+    exit 1
+fi
+
+REFSEQ_GTF="$DATA_DIR"/refseq/Mus_musculus.GRCm39.115.gtf.gz
+
+#Check if there is REFSEQ_FASTA file 
+if [ ! -f "$REFSEQ_GTF" ]; then 
+    echo "Please provide the refseq (gtf file) in this path :: $REFSEQ_GTF"
+    exit 1
+fi
+
+REFSEQ_GFF3="$DATA_DIR"/refseq/Mus_musculus.GRCm39.115.gff3.gz
+
+#Check if there is REFSEQ_FASTA file 
+if [ ! -f "$REFSEQ_GFF3" ]; then 
+    echo "Please provide the refseq (gff3 file) in this path :: $REFSEQ_GFF3"
+    exit 1
+fi
 
 ##------------------------------------------------------------------------
 ## Set up path for results and logs
