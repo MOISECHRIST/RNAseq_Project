76b1d68d587a6700f2429a78b37673441f874a22
MOISE CHRISTIAN JUNIOR MEKA <115233333+MOISECHRIST@users.noreply.github.com>
2025-11-20 20:56:08 +0100

    Data exploration done

--- scripts/13-Run_R_data_analysis.R
+++ scripts/13-Run_R_data_analysis.R
@@ -12,31 +12,43 @@
 ## Last Update : 20-11-2025
 ##------------------------------------------------------------------------
 
-#Import libraries 
+##------------------------------------------------------------------------
+## Step 1 : Set Working Directory
+##------------------------------------------------------------------------
+setwd("~/Documents/Msc_Bioinf_UniBern/Master-1/Semester-1/RNA Seq Analysis/project/RNAseq_Project")
+
+##------------------------------------------------------------------------
+## Step 2 : Import libraries 
+##------------------------------------------------------------------------
 library(conflicted)
 library("DESeq2")
 library("clusterProfiler")
 library("vsn")
 library("pheatmap")
-
-#Load data
+library(ggplot2)
+##------------------------------------------------------------------------
+## Step 3 : Load data
+##------------------------------------------------------------------------
 load("results/summary/DE_Analysis/differential_expression_model.RData")
 gene.detail <- read.csv("results/summary/DE_Analysis/Mus_musculus.GRCm39.115.csv", 
                         sep=",", header = T)
 
 #A glimpse of our data 
 str(dds)
+resultsNames(dds)
 str(gene.detail)
 
-#Data transformations
-##Variance-Stabilizing transformation 
-vsd <- vst(dds, blind=FALSE)
+##------------------------------------------------------------------------
+## Step 4 : Data exploration 
+##------------------------------------------------------------------------
 
-##Regularized log transformation
-rld <- rlog(dds, blind=FALSE)
+# Data transformations
+## Variance-Stabilizing transformation 
+vsd <- vst(dds, blind=TRUE)
 
-##this gives log2(n + 1)
-ntd <- normTransform(dds)
+## Regularized log transformation
+rld <- rlog(dds, blind=TRUE)
+
 
 pdf("results/summary/DE_Analysis/plots/vst_data_transformation.pdf")
 meanSdPlot(assay(vsd))
@@ -46,20 +58,14 @@ pdf("results/summary/DE_Analysis/plots/rlog_data_transformation.pdf")
 meanSdPlot(assay(rld))
 dev.off()
 
-pdf("results/summary/DE_Analysis/plots/ntd_data_transformation.pdf")
-meanSdPlot(assay(ntd))
-dev.off()
 
-#Data visualization : QC Check
-max_val <- 30
+# Data visualization 
+
+## QC Check
+max_val <- 80
 select <- order(rowMeans(counts(dds,normalized=TRUE)),
                 decreasing=TRUE)
 df <- as.data.frame(colData(dds)[,c("batch","condition")])
-
-pdf("results/summary/DE_Analysis/plots/ntd_qc_check.pdf")
-pheatmap(assay(ntd)[select[1:max_val],], cluster_rows=FALSE, show_rownames=FALSE,
-         cluster_cols=T, annotation_col=df)
-dev.off()
 
 pdf("results/summary/DE_Analysis/plots/vst_qc_check.pdf")
 pheatmap(assay(vsd)[select[1:max_val],], cluster_rows=FALSE, show_rownames=FALSE,
@@ -69,6 +75,19 @@ dev.off()
 pdf("results/summary/DE_Analysis/plots/rlog_qc_check.pdf")
 pheatmap(assay(rld)[select[1:max_val],], cluster_rows=FALSE, show_rownames=FALSE,
          cluster_cols=T, annotation_col=df)
+dev.off()
+
+## PCA 
+
+pdf("results/summary/DE_Analysis/plots/vst_plotPCA.pdf")
+plotPCA(vsd, intgroup=c("condition", "batch"), 
+                   ntop=length(select))
+dev.off()
+
+
+pdf("results/summary/DE_Analysis/plots/rlog_plotPCA.pdf")
+plotPCA(rld, intgroup=c("condition", "batch"), 
+                   ntop=length(select))
 dev.off()
 
 
