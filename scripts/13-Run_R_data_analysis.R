4db6d99cb96415d23c0283e23e685d3256b49ef1
MOISE CHRISTIAN JUNIOR MEKA <115233333+MOISECHRIST@users.noreply.github.com>
2025-11-20 13:26:17 +0100

    R script for data analysis, data exploration and overrepresented analysis

---
+++ scripts/13-Run_R_data_analysis.R
@@ -0,0 +1,53 @@
+#!/usr/local/bin/R 
+
+##------------------------------------------------------------------------
+## Author : MEKA Moise
+## Email : moise.meka@students.unibe.ch
+## Description : This script is for data analysis. 
+#           Here we take as input ".RData" object from script 09 and then do :
+##              -> Data exploration,
+##              -> Extract and analyse DE results,
+##              -> Overrepresentation analysis.
+## Creation date : 20-11-2025
+## Last Update : 20-11-2025
+##------------------------------------------------------------------------
+
+#Import libraries 
+library(conflicted)
+library("DESeq2")
+library("clusterProfiler")
+library("vsn")
+
+#Load data
+load("results/summary/DE_Analysis/differential_expression_model.RData")
+gene.detail <- read.csv("results/summary/DE_Analysis/Mus_musculus.GRCm39.115.csv", 
+                        sep=",", header = T)
+
+#A glimpse of our data 
+str(dds)
+str(gene.detail)
+
+#Data transformations
+##Variance-Stabilizing transformation 
+vsd <- vst(dds, blind=FALSE)
+
+##Regularized log transformation
+rld <- rlog(dds, blind=FALSE)
+
+##this gives log2(n + 1)
+ntd <- normTransform(dds)
+
+pdf("results/summary/DE_Analysis/plots/vst_data_transformation.pdf")
+meanSdPlot(assay(vsd))
+dev.off()
+
+pdf("results/summary/DE_Analysis/plots/rlog_data_transformation.pdf")
+meanSdPlot(assay(rld))
+dev.off()
+
+pdf("results/summary/DE_Analysis/plots/ntd_data_transformation.pdf")
+meanSdPlot(assay(ntd))
+dev.off()
+
+
+
